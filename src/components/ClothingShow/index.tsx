4b31cd876b6eca1562b0c060c4a20c51d3e85802
buffoon <191427qp@gmail.com>
2023-02-04 19:16:23 +0800

    放大镜组件完成

--- src/components/ClothingShow/index.tsx
+++ src/components/ClothingShow/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {  useRef, useState } from "react";
 import { Magnifier } from "../../utils/magnifier";
 import s from './index.module.less'
 
@@ -8,31 +8,34 @@ type Props = {
 
 const ClothingShow: React.FC<Props> = ({ images }) => {
   const imgRef = useRef<HTMLImageElement|null>(null)
+  const [imgIndex,setImgIndex] = useState(0)
 
-  useEffect(() => {
-    new Magnifier({
-      targit: imgRef.current!,
+  const initMagnifier = () => {
+    // init magnifier
+     new Magnifier({
+      target: imgRef.current!,
       scale: 3,
       magnifierSize:300
     })
-  },[])
-
-  console.log("images", images);
-  const [imgIndex,setImgIndex] = useState(0)
+  }
 
   return <div className={s.clothing}>
     <div className={s.clothing_box}>
-      <img className={s.clothing_box_img} src={images[imgIndex]} alt="img" ref={imgRef}/>
-      <div className={s.clothing_box_cover}></div>
+      <img
+        className={s.clothing_box_img}
+        src={images[imgIndex]}
+        alt="img"
+        onClick={() => initMagnifier()}
+        ref={imgRef}
+      />
     </div>
-   
     <div className={s.clothing_mini}>
       {
         images.slice(0,5).map((item,index) => {
           return <img
             className={s.clothing_mini_img}
             src={item} alt="image"
-            key={item}
+            key={index}
             onClick={() => setImgIndex(index)}
             onMouseOver={()=>setImgIndex(index)}  
           />
--- src/utils/magnifier.ts
+++ src/utils/magnifier.ts
@@ -1,21 +1,22 @@
 type MagnifierType = {
-  targit: HTMLImageElement,
+  target: HTMLImageElement,
   scale: number,
   magnifierSize:number
 }
 
 class Magnifier {
-  targit: HTMLImageElement
+  target: HTMLImageElement
   scale:number
   magnifierSize: number
-  private magnifier:HTMLDivElement | null = null
+  private magnifier: HTMLDivElement | null = null
+  private showMagnifier:boolean = false
   
-  constructor({ targit,scale,magnifierSize}:MagnifierType) {
-    // 初始化targit
-    if (targit instanceof HTMLImageElement) {
-      this.targit = targit
+  constructor({ target,scale,magnifierSize}:MagnifierType) {
+    // 初始化target
+    if (target instanceof HTMLImageElement) {
+      this.target = target
     } else {
-      throw new Error('targit is not a HTMLImageElement')
+      throw new Error('target is not a HTMLImageElement')
     }
 
     // 初始化scale
@@ -38,31 +39,99 @@ class Magnifier {
 
   private init = () => {
     this.createMagnifier()
-    this.hanleEvent()
+    this.handleEvents()
   }
 
   private createMagnifier = () => {
     const magnifier = document.createElement('div')
     magnifier.style.cssText = `
       position:fixed;
-      top:50%;
-      left:50%;
+      top:300px;
+      left:400px;
       width:${this.magnifierSize}px;
       height:${this.magnifierSize}px;
       border-radius:50%;
       overflow:hidden;
       box-shadow:inset 0 0 20px #000; 
-      background:url(${this.targit.src}) no-repeat #ccc;
-      background-size:${this.targit.clientWidth * this.scale}px auto; 
+      background:url(${this.target.src}) no-repeat #ccc;
+      background-size:${this.target.clientWidth * this.scale}px auto; 
     `
     document.body.append(magnifier)
     this.magnifier = magnifier
   }
 
-  private hanleEvent = () => {
-    
+  private handleEvents = () => {
+    // toggle magnifier when click
+    document.addEventListener('click', (e) => {
+      const { clientX, clientY } = e;
+      if (e.target === this.target) {
+        // if click on target image, toggle
+        this.showMagnifier = !this.showMagnifier;
+      } else {
+        // if click elsewhere, hide
+        this.showMagnifier = false;
+      }
+      this.toggleMagnifier();
+      if (this.showMagnifier) {
+        this.updateMagnifier(clientX, clientY);
+      }
+    });
+
+    // move magnifier
+    document.addEventListener('mousemove', (e) => {
+      if (!this.showMagnifier) return;
+      const { clientX, clientY } = e;
+      if (this.isInTarget(clientX, clientY)) {
+        this.updateMagnifier(clientX, clientY);
+      } else {
+        // hide magnifier when move out of target image
+        this.showMagnifier = false;
+        this.toggleMagnifier();
+      }
+    });
+
+    // reset magnifier's background-size after window resize
+    window.addEventListener('resize', (e) => {
+      this.magnifier!.style.backgroundSize = `${this.target.clientWidth * this.scale}px auto`;
+    });
   }
 
+  private toggleMagnifier = () => {
+    this.magnifier!.style.display = this.showMagnifier ? 'block' : 'none';
+    // set pointer to crosshair to better target zoom area
+    this.target.style.cursor = this.showMagnifier ? 'crosshair' : 'unset';
+  }
+
+  /**
+   * update magnifier's position and background image
+   */
+  private updateMagnifier = (x:number, y:number) => {
+    if (x + this.magnifierSize > window.innerWidth) {
+      // keep magnifier in viewport
+      this.magnifier!.style.left = window.innerWidth - this.magnifierSize + 'px';
+    } else {
+      this.magnifier!.style.left = x + 'px';
+    }
+    if (y + this.magnifierSize > window.innerHeight) {
+      // keep magnifier in viewport
+      this.magnifier!.style.top = window.innerHeight - this.magnifierSize + 'px';
+    } else {
+      this.magnifier!.style.top = y + 'px';
+    }
+    const targetRect = this.target.getBoundingClientRect();
+    const bgX = -(x - targetRect.x) * this.scale + this.magnifierSize / 2 + 'px';
+    const bgY = -(y - targetRect.y) * this.scale + this.magnifierSize / 2 + 'px';
+    this.magnifier!.style.backgroundPosition = `${bgX} ${bgY}`;
+  }
+
+  private isInTarget = (x:number, y:number) => {
+    const targetRect = this.target.getBoundingClientRect();
+    const { left, top, width, height } = targetRect;
+    if (x < left || y < top || x > left + width || y > top + height) {
+      return false;
+    }
+    return true;
+  }
 }
 
 export {
